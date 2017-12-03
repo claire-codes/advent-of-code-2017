6c16688141ad34621754d09cbbdf7a176f44111e
Claire Parker <claireparker@users.noreply.github.com>
2017-12-03 20:12:53 +0000

    :no_good: Day 1 Part 1

--- code/day-one.js
+++ code/day-one.js
@@ -1,7 +1,23 @@
 module.exports = (function() {
   return {
-    captcha: function() {
-        return 3;
+    captcha: function(code) {
+        // assume a string cos easier to split,
+        // then parse to integer when necessary
+        var numbers = code.split('');
+        var runningTotal = 0;
+        var lastOne = numbers.length - 1;
+        numbers.forEach(function(number, index) {
+            if (index !== lastOne) {
+                if (number === numbers[index + 1]) {
+                    runningTotal += parseInt(number);
+                }
+            } else {
+                if (number === numbers[0]) {
+                    runningTotal += parseInt(number);
+                }
+            }
+        });
+        return runningTotal;
     }
   }
 })();
